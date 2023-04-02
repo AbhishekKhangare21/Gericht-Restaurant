8f3833716bb3bb3f689d4b761798090a934166ef
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-04-02 06:08:00 +0530

    favicon and footer changed

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
-
 import { FooterOverlay, Newsletter } from "../../components";
 import { images } from "../../constants";
 import "./Footer.css";
@@ -13,9 +12,12 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">
+          Madhav Kunj Sodawala Lane Corner S V P Road Borivali ,
+          Mumbai,400091,India
+        </p>
+        <p className="p__opensans">+91 212-344-1230</p>
+        <p className="p__opensans">+91 212-555-1230</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -46,7 +48,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2023 AK. All Rights reserved.</p>
+      <p className="p__opensans">2023 Gericht. All Rights reserved.</p>
     </div>
   </div>
 );
